128d859cd4e27d2c6ed5f9a5f784a2151962c77f
Yu-Hsiang Lin <hitr2997925@gmail.com>
2017-04-01 15:08:56 +0800

    Revised rebuild.sh. Created ref.txt for convenience.

--- project1/code/rebuild.sh
+++ project1/code/rebuild.sh
@@ -5,13 +5,13 @@
 # Will not shutdown and reboot
 
 # Directory of Ubuntu source files
-dir = usr/src/linux-2.6.32.60
+dir=/usr/src/linux-2.6.32.60
 
 # Enter the number of cores to use
-read -p "\nEnter the number of cores you want to use for rebuild: " ncore
+read -p 'Enter the number of cores you want to use for rebuild: ' ncore
 
 # Rebuild
-sudo $(dir)/make -j$(ncore) bzImage
-#sudo $(dir)/make -j$(ncore) modules
-#sudo $(dir)/make -j$(ncore) modules_install
-#sudo $(dir)/make -j$(ncore) install
+sudo make -j${ncore} -C ${dir} bzImage
+sudo make -j${ncore} -C ${dir} modules
+sudo make -j${ncore} -C ${dir} modules_install
+sudo make -j${ncore} -C ${dir} install
